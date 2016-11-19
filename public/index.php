95fb01d7e2ea4706ec000098014dd60c2fdfc0c7
Roger Riera Rojas <rogeriera@hotmail.com>
2016-11-19 01:48:00 +0000

    Final commit

--- public/index.php
+++ public/index.php
@@ -1,8 +1,7 @@
 <?php
+    use Particle\Validator\Validator;
 
     require_once '../vendor/autoload.php';
-
-    echo "Hello World";
 
     $file = '../storage/database.db';
     if (is_writable('../storage/database.local.db')) {
@@ -14,11 +13,34 @@
     ]);
 
     $comment = new SitePoint\Comment($database);
-    $comment->setEmail('bruno@skvorc.me')
-        ->setName('Bruno Skvorc')
-        ->setComment('Hooray! Saving comments works!')
-        ->save();
-        dump($database->error());
+    /*$comment->setEmail('bruno@skvorc.me')
+    ->setName('Bruno Skvorc')
+    ->setComment('Hooray! Saving comments works!')
+    ->save();
+    dump($database->error());*/
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $v = new Validator();
+        $v->required('name')->lengthBetween(1, 100)->alnum(true);
+        $v->required('email')->email()->lengthBetween(5, 255);
+        $v->required('comment')->lengthBetween(10, null);
+        $result = $v->validate($_POST);
+        if ($result->isValid()) {
+            try {
+                $comment
+                    ->setName($_POST['name'])
+                    ->setEmail($_POST['email'])
+                    ->setComment($_POST['comment'])
+                    ->save();
+                header('Location: /');
+                return;
+            } catch (\Exception $e) {
+                die($e->getMessage());
+            }
+        } else {
+            dump($result->getMessages());
+        }
+    }
 
 ?>
 
@@ -36,12 +58,20 @@
 
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/custom.css">
         <script src="js/vendor/modernizr-2.8.3.min.js"></script>
     </head>
     <body>
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
+
+        <?php foreach ($comment->findAll() as $comment) : ?>
+            <div class="comment">
+                <h3>On <?= $comment->getSubmissionDate() ?>, <?= $comment->getName() ?> wrote:</h3>
+                <p><?= $comment->getComment(); ?></p>
+            </div>
+        <?php endforeach; ?>
 
         <!-- Add your site or application content here -->
         <form method="post">
